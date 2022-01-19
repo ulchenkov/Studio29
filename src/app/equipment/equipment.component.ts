34aa7dd6ff483a1d66e02f223fa0c3b337cb6979
Vitaly Ulchenkov <github@ulchenkov.com>
2022-01-19 14:55:09 -0500

    Completed bonus missions

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipments:string[] = ["Habitat dome", "Drones", "Food containers", "Oxygen tanks"];
+  editing:string = "";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem(newItem: string): void {
+    if (newItem.trim() === "" || this.equipments.indexOf(newItem) >= 0) {
+      return;
+    }
+    this.equipments.push(newItem);
+  }
+
+  editItem (oldName:string, newName: string): void {
+    if (newName.trim() === "" || this.equipments.indexOf(oldName) < 0) {
+      return;
+    }
+    this.equipments.splice(this.equipments.indexOf(oldName), 1, newName);
+    this.editing = "";
+  }
+
+  removeItem(item: string) {
+    this.equipments.splice(this.equipments.indexOf(item), 1);
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experements:string[] = ["Mars soil sample", "Plant growth in habitat", "Human bone density"];
+
+  editing:string = "";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem(newItem: string): void {
+    if (newItem.trim() === "" || this.experements.indexOf(newItem) >= 0) {
+      return;
+    }
+    this.experements.push(newItem);
+  }
+
+  editItem (oldName:string, newName: string): void {
+
+    if (newName.trim() === "" || this.experements.indexOf(oldName) < 0) {
+      return;
+    }
+    this.experements.splice(this.experements.indexOf(oldName), 1, newName);
+    this.editing = "";
+  }
+
+  removeItem(item: string) {
+    this.experements.splice(this.experements.indexOf(item), 1);
   }
 
 }
